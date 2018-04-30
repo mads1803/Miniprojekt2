db3070a57902a756ff6acb8d7788b42b5373ba17
Simon Tofteby <simontofteby@gmail.com>
2018-04-30 12:14:12 +0200

    weblink stufff

--- app/src/main/java/com/example/madsstoltenborg/rejsedagbog/Weblink.java
+++ app/src/main/java/com/example/madsstoltenborg/rejsedagbog/Weblink.java
@@ -1,14 +1,17 @@
 package com.example.madsstoltenborg.rejsedagbog;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.Toast;
 
-public class weblink extends AppCompatActivity {
+public class Weblink extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,23 @@ public class weblink extends AppCompatActivity {
 
         WebView myWebView = (WebView) findViewById(R.id.note_webview);
         //TODO: Fixe HTTP her
-        myWebView.loadUrl("www.example.com");
+    String newWeblink = "http://www.example.com";
+        try {
+         String url = getIntent().getStringExtra("weblink");
+            String[] web = url.split("www");
+
+             newWeblink = "http://www" + web[1];
+        }
+        catch (Exception e ) {
+            Context context = getApplicationContext();
+            CharSequence text = "Invalid URL, here is a an Example";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        myWebView.loadUrl(newWeblink);
     }
 
     @Override
